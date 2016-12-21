5cbed9c77d344bc55820092d3f7ed19d21c2e1dc
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-12-21 11:16:48 -0700

    Oops. Typo

--- components/SitePost/PostFooter.js
+++ components/SitePost/PostFooter.js
@@ -8,7 +8,7 @@ export default function PostFooter () {
     <div className='footer'>
       <img className='avatar' src={profilePic} />
       <div className='author-bio'>
-        <p>I'm Tyler. I'm a <a href="https://developers.google.com/experts/people/tyler-mcginnis">Google Developer Export</a> and a partner at <a href="https://reacttraining.com">React Training</a> where we teach React online, in person, and work on OSS like React Router.</p>
+        <p>👋 I'm Tyler. I'm a <a href="https://developers.google.com/experts/people/tyler-mcginnis">Google Developer Expert</a> and a partner at <a href="https://reacttraining.com">React Training</a> where we teach React online, in person, and work on OSS like React Router.</p>
         <a href="https://twitter.com/tylermcginnis33" className="twitter-follow-button" data-show-count="true">Follow @tylermcginnis33</a>
       </div>
     </div>
