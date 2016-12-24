d86098a79307a3741a7e6edea987bfba0dfd0c6f
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-12-23 18:11:27 -0700

    Style tweaks

--- components/SitePost/AboutMe.js
+++ components/SitePost/AboutMe.js
@@ -13,12 +13,16 @@ AboutMe.defaultProps = {
 
 export default function AboutMe ({top}) {
   return (
-    <div className='footer' style={{marginTop: top === true ? 0 : null, borderWidth: top === true ? 0 : null}}>
+    <div>
+      { top === true ? null : <hr style={{marginBottom: 30}} /> }
+      <div className='footer'>
       <img className='avatar' src={profilePic} />
       <div className='author-bio'>
         <p>👋 I'm Tyler. I'm a <a href="https://developers.google.com/experts/people/tyler-mcginnis">Google Developer Expert</a> and a partner at <a href="https://reacttraining.com">React Training</a> where we teach React online, in person, and work on OSS like React Router.</p>
         <a href="https://twitter.com/tylermcginnis33" className="twitter-follow-button" data-show-count="true">Follow @tylermcginnis33</a>
       </div>
+      </div>
+      { top === true ? null : <div style={{height: 62}} /> }
     </div>
   )
 }
