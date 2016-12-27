7d5933ac1f8776be584e7a5f29400121658d003d
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-12-26 20:54:31 -0700

    robots and sitemap

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,20 +3,65 @@ var lost = require("lost")
 var cssnext = require("postcss-cssnext")
 
 exports.modifyWebpackConfig = function(config, env) {
-    config.merge({
-        postcss: [
-            lost(),
-            rucksack(),
-            cssnext({
-                browsers: ['>1%', 'last 2 versions']
-            })
-        ]
+  config.merge({
+    postcss: [
+      lost(),
+      rucksack(),
+      cssnext({
+        browsers: ['>1%', 'last 2 versions']
+      })
+    ]
+  })
+
+  config.loader('svg', {
+    test: /\.(svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader',
+  })
+
+  return config
+}
+
+import fs from 'fs-extra-promise'
+import sm from 'sitemap'
+
+const sitename = 'https://tylermcginnis.com'
+
+function generateSiteMap(pages) {
+
+  const urls = pages.filter((page) => page.path !== undefined)
+    .map((page) => {
+      if (page.path === '/') {
+        return {
+          url: sitename + page.path,
+          changefreq: 'weekly',
+          priority: 1,
+          lastmodISO: new Date().toISOString()
+        }
+      }
+
+      return {
+        url: sitename + page.path,
+        changefreq: 'monthly',
+        priority: 0.5,
+        lastmodISO: page.data.date || new Date().toISOString()
+      }
     })
 
-    config.loader('svg', {
-       test: /\.(svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-       loader: 'file-loader',
-    })
+    console.log(urls)
 
-    return config
-};
+  const sitemap = sm.createSitemap({
+    hostname: 'https://tylermcginnis.com',
+    cacheTime: '60000',
+    urls
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+}
+
+export function postBuild(pages, callback) {
+  generateSiteMap(pages)
+  callback()
+}
