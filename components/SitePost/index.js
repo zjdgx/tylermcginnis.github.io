5a3cd4a848056fbf10797e24ed6cf20a5a019a61
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-12-28 23:59:43 -0700

    Add social btns

--- components/SitePost/index.js
+++ components/SitePost/index.js
@@ -5,16 +5,18 @@ import { prefixLink } from 'gatsby-helpers'
 import './style.css'
 import '../../static/css/highlight.css'
 import AboutMe from './AboutMe'
-import Helmet from "react-helmet"
+import Helmet from 'react-helmet'
+import SocialShare from './SocialShare'
 
 export default function SitePost (props) {
   const { title, date, body } = props.route.page.data
+  const url = `https://tylermcginnis.com${props.route.page.path}`
 
   return (
     <div>
       <Helmet
         title={title}
-        link={[{rel: "canonical", href: `https://tylermcginnis.com/${props.route.page.path}`}]} />
+        link={[{rel: "canonical", href: url}]} />
       <Link className='gohome' to={ prefixLink('/') }>
         Back
       </Link>
@@ -24,6 +26,10 @@ export default function SitePost (props) {
           <h1 className='blog-title'>{title}</h1>
           <div className='date'>{moment(date).format('MMM D, YYYY')}</div>
           <div className='blog-body' dangerouslySetInnerHTML={{__html: body}} />
+        </div>
+        <div style={{marginTop: 50}}>
+          <SocialShare type='twitter' tweet={`"${title}" by @tylermcginnis33`} url={url} />
+          <SocialShare type='facebook' url={url} />
         </div>
         <AboutMe />
       </div>
